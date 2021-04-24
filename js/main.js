200fd54dabcd65842cf7b050de922f56a835b1fd
Ankita Goyal <ankita@Ankitas-MacBook-Pro.local>
2021-04-24 20:27:19 +0530

    index page

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.main-banner').owlCarousel({
+    loop: true,
+    autoplay: true,
+    nav: false,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
